5df73812421bb84f68d5434c85cfde3dea443a91
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-10 12:45:47 +0000

    Add swift API helpers

---
+++ OSJSON/OSJSON.swift
@@ -0,0 +1,27 @@
+//
+//  OSJSON.swift
+//  OSJSON
+//
+//  Created by Dave Hardiman on 10/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+import Foundation
+
+/// Shorthand typealias
+public typealias JSON = OSJSON
+
+/**
+ *  Represents an entity that can be decoded from a JSON object
+ */
+public protocol Decodable {
+
+    /**
+     Optional initialiser that can create an entity from the passed in JSON
+
+     - parameter json: The JSON to use to create the object
+
+     - returns: An initialised entity or nil if decoding failed
+     */
+    init?(json: JSON)
+}
