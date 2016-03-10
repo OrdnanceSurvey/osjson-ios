8647d230852196527100ab26099b1ab89a4edfbe
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-10 10:29:58 +0000

    Add xcode project

---
+++ OSJSON/OSJSON.h
@@ -0,0 +1,19 @@
+//
+//  OSJSON.h
+//  OSJSON
+//
+//  Created by Dave Hardiman on 10/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSJSON.
+FOUNDATION_EXPORT double OSJSONVersionNumber;
+
+//! Project version string for OSJSON.
+FOUNDATION_EXPORT const unsigned char OSJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSJSON/PublicHeader.h>
+
+
